377e37b874ed684a97d24502faae8cf588d18ef2
Wayve <lolwr0200@gmail.com>
2025-10-31 09:14:56 +0530

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,43 @@
+// This file creates the generative art disguise.
+let particles = [];
+
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('__main'); // Use a non-descriptive parent
+    for (let i = 0; i < 100; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function draw() {
+    background(0, 10); // Black background with a slight trail effect
+    for (let particle of particles) {
+        particle.update();
+        particle.show();
+    }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+class Particle {
+    constructor() {
+        this.pos = createVector(random(width), random(height));
+        this.vel = p5.Vector.random2D().mult(random(0.5, 2));
+        this.size = random(2, 5);
+        this.color = color(random(100, 255), random(100, 255), random(100, 255), 150);
+    }
+
+    update() {
+        this.pos.add(this.vel);
+        if (this.pos.x > width || this.pos.x < 0) this.vel.x *= -1;
+        if (this.pos.y > height || this.pos.y < 0) this.vel.y *= -1;
+    }
+
+    show() {
+        noStroke();
+        fill(this.color);
+        ellipse(this.pos.x, this.pos.y, this.size);
+    }
+}
